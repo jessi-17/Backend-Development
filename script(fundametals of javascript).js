975553fe275035222f8f2e7b2fed5b5ef3060141
jessi-17 <jaspreet.bhagat17@gmail.com>
2024-10-10 16:10:06 +0530

    done with fundamentals and then asynchronous

---
+++ script(fundametals of javascript).js
@@ -0,0 +1,13 @@
+var arr = [1,2,3,4,"hey",{},true,function(){},[]]
+//put any kind of value in arr in js. 
+
+
+var arr1 = [1,2,3,4];
+
+//things to knwo about: foreach map filter map indexof
+
+
+// for each member of the element in the arr
+arr1.forEach(function(val){
+    console.log(val + "hello"); //val:-1,2,3,4
+})
